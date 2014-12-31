5fb3b53bbb10c2f6c5c2aa8681aaad713d027da5
maryhipp <maryhipp@gmail.com>
2014-12-30 21:53:12 -0500

    single blog and case study page

--- wp-content/themes/adaptly/page-blog.php
+++ wp-content/themes/adaptly/page-blog.php
@@ -5,7 +5,7 @@
   </div>
 <?php endwhile; wp_reset_query(); ?>
 
-  <div class="resources subnav mid-cont">
+  <div class="resources subnav mid-cont blog">
     <ul>
       <li class="blog"><a href=<?php site_url(); ?>'/resources/blog' class='active'>Blog</a></li>
       <li class="case-studies"><a href=<?php site_url(); ?>'/resources/case-studies'>Case Studies</a></li>
@@ -34,10 +34,23 @@
       </div>
       <?php endwhile;  ?>
       <div class="pagination">
-        <?php previous_posts_link( '< Newer Entries' ); ?>
-        <?php next_posts_link( 'Older Entries >', 0 ); wp_reset_postdata(); ?>
+        <?php previous_posts_link( 'Newer >' ); ?>
+        <?php next_posts_link( '> Older', 0 ); wp_reset_postdata(); ?>
       </div>
   </div>
+</div>
+
+<div class="pre-footer-cta mobile">
+  <p class="newsletter-signup">Start receiving Adaptly emails weekly</p>
+  <form action="//adaptly.us3.list-manage.com/subscribe/post?u=1304346d0242a3209ad4480f9&amp;id=db19f9e852" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate mailchimp-signup" target="_blank" novalidate>
+    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Enter Email">
+    <div id="mce-responses" class="clear">
+      <div class="response" id="mce-error-response" style="display:none"></div>
+      <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_1304346d0242a3209ad4480f9_db19f9e852" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Go" name="subscribe" id="mc-embedded-subscribe" class="button" ></div>
+  </form>
 </div>
 
 <?php
