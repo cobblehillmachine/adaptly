2e925bbe0aeaa7c15a3f22cdb271fe8533404fea
maryhipp <maryhipp@gmail.com>
2015-01-13 16:44:11 -0500

    mobile

--- wp-content/themes/adaptly/js/adaptly.js
+++ wp-content/themes/adaptly/js/adaptly.js
@@ -15,6 +15,7 @@ $(window).load(function() {
   });
   if ($(window).width() > 450px) {
     homepageAnimation();
+    var height = $(window).height();
     $('.animation-text-wrapper').css('height', height + 179);
     $('.home.page footer').show();
     clientTestimonialSlider();
