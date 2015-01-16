dd3d2a48ccab978d317575b7f1b83d2aeb3e9e65
Mary Hipp <maryhipp@gmail.com>
2015-01-16 11:32:22 -0500

    reconfiguring blog post author

--- wp-content/themes/adaptly/single.php
+++ wp-content/themes/adaptly/single.php
@@ -34,10 +34,10 @@ get_header(); ?>
   if ($author) { ?>
 <div class="author-wrapper">
   <div class="author mid-cont blog-post">
-    <div class="author-name mobile"><h4> By <?php the_field('author_name'); ?></h4></div>
+    <div class="author-name mobile"><h4><strong> By <?php the_field('author_name'); ?></strong></h4><h4<?php the_field('author_title'); ?></h4></div>
     <img src="<?php the_field('author_image'); ?>" class="headshot">
     <div class="author-info">
-      <div class="author-name desktop"><?php the_field('author_name'); ?></div>
+      <div class="author-name desktop"><?php the_field('author_name'); ?>, <?php the_field('author_title'); ?></div>
       <?php the_field('author_bio'); ?>
       <?php $has_facebook = get_field('author_facebook_link');
       $has_twitter = get_field('author_twitter_link');
