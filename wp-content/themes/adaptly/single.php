37d5be085f72bbb616f7dd3980b065ca9df8dd59
maryhipp <maryhipp@gmail.com>
2015-06-15 11:09:53 -0400

    pinterest js on blog post

--- wp-content/themes/adaptly/single.php
+++ wp-content/themes/adaptly/single.php
@@ -73,4 +73,13 @@ get_header(); ?>
   </form>
 </div>
 <?php endwhile; wp_reset_query(); ?>
+<script type="text/javascript">
+(function(d){
+    var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+    p.type = 'text/javascript';
+    p.async = true;
+    p.src = '//assets.pinterest.com/js/pinit.js';
+    f.parentNode.insertBefore(p, f);
+}(document));
+</script>
 <?php get_footer(); ?>
